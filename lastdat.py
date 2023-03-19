7b2f3f09695a46aa3e2aad1c5e82aa1844d0ea4c
Repinoid <tazetdinovn@gmail.com>
2023-03-19 08:36:40 +0400

    w gldt

--- lastdat.py
+++ lastdat.py
@@ -1,18 +1,25 @@
 import requests
 import re
 
-url = "https://russian-trade.com/coronavirus-russia/moskva/"
-response = requests.get(url)
-txt = response.text  # Access the HTML with the text property
+def getLastCodidDate(url = "https://russian-trade.com/coronavirus-russia/moskva/"):
 
-patter = '<table\s+id="curs_special".+<tr><td>\d{2}.\d{2}.\d{4}</td>'
-lastDataCell = "<tr><td>\d{2}.\d{2}.\d{4}</td>"
+    response = requests.get(url)
+    txt = response.text  # Access the HTML with the text property
 
-tabloidPosition = re.search(patter, txt).regs[0][0]
-lastDataCellPosition = re.search(lastDataCell, txt[tabloidPosition:])
-lastDatBeg = tabloidPosition+lastDataCellPosition.regs[0][0] + len('<tr><td>')
-lastDatEnd = tabloidPosition+lastDataCellPosition.regs[0][1] - len('</td>')
-cus = txt[lastDatBeg:lastDatEnd]
-datta = f"{cus[6:10]}-{cus[3:5]}-{cus[0:2]}"
+    patter = '<table\s+id="curs_special".+<tr><td>\d{2}.\d{2}.\d{4}</td>'
+    lastDateCell = "<tr><td>\d{2}.\d{2}.\d{4}</td>"
 
-print (datta)
+    tabloidPosition = re.search(patter, txt).regs[0][0]
+    lastDataCellPosition = re.search(lastDateCell, txt[tabloidPosition:])
+    lastDatBeg = tabloidPosition+lastDataCellPosition.regs[0][0] + len('<tr><td>')
+    lastDatEnd = tabloidPosition+lastDataCellPosition.regs[0][1] - len('</td>')
+    cus = txt[lastDatBeg:lastDatEnd]
+    dateS = {}
+    try:
+        dateS["Year"]   = int(cus[6:10])
+        dateS["Month"]  = int(cus[3:5])
+        dateS["Day"]    = int(cus[0:2])
+    except ValueError:
+        dateS = None
+    return dateS
+
